76620ae817898413f5271ea51158e7c3148df46e
Otavio Oshiro <otavioshiro@msn.com>
2020-07-15 15:57:29 -0400

    Criados os métodos de lista

--- index.php
+++ index.php
@@ -4,16 +4,31 @@ require_once("config.php");
 
 /*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
 
+/*Carrega um usuário
 $root = new Usuario();
-
 $root->loadbyId(3);
-
 echo $root;
+*/
+
+
+/*Carrega uma lista de usuários
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/*Carrega uma lista de usuáricos buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+//*Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Ana", "112233");
+
+echo $usuario;
 
  ?>
